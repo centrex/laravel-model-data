a7d019aff97185ec1f2019f7081acfd4361f1988
rochi88 <raisul.me@gmail.com>
2023-11-15 17:27:34 +0600

    refresh code

--- src/ModelDataServiceProvider.php
+++ src/ModelDataServiceProvider.php
@@ -14,29 +14,30 @@ class ModelDataServiceProvider extends ServiceProvider
         /*
          * Optional methods to load your package assets
          */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'model-data');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'model-data');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-model-data');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-model-data');
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('model-data.php'),
+                __DIR__.'/../config/config.php' => config_path('laravel-model-data.php'),
             ], 'config');
 
             // Publishing the views.
             /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/model-data'),
+                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-model-data'),
             ], 'views');*/
 
             // Publishing assets.
             /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/model-data'),
+                __DIR__.'/../resources/assets' => public_path('vendor/laravel-model-data'),
             ], 'assets');*/
 
             // Publishing the translation files.
             /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/model-data'),
+                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laracvel-model-data'),
             ], 'lang');*/
 
             // Registering package commands.
@@ -48,10 +49,10 @@ class ModelDataServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'model-data');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-model-data');
 
         // Register the main class to use with the facade
-        $this->app->singleton('model-data', function () {
+        $this->app->singleton('laravel-model-data', function () {
             return new Data;
         });
     }
